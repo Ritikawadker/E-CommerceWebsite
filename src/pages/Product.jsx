9978eb6b2510293df42f0dc495b174cda321efae
ritikInnogent <ritik.kawadker@innogent.in>
2025-11-07 11:54:42 +0530

    updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/Product.css'; 
+import "../styles/Product.css"
 
 const Product = () => {
   const { id } = useParams();  
